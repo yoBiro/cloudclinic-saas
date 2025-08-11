f97b8f9abd7d5733eade4c9d8fb39c4d3c7b694b
yoBiro <bielzingg92@gmail.com>
2025-08-11 00:49:28 -0300

    Sétimo Commit - 3 Interfaces realizadas e seguindo ao fim do projeto | Aprendendo JS para a criação dos Modals

--- src/types/modals.js
+++ src/types/modals.js
@@ -1,7 +1,17 @@
-export function openModal(setModalState) {
-    setModalState(true);
+const modal = getElementById("myModal");
+const openModal = getElementById("openModal");
+const closeModal = getElementById(".close");
+
+function openModal() {
+    modal.style.display = "block";
 }
 
-export function closeModal(setModalState) {
-    setModalState(false);
+closeModal.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
